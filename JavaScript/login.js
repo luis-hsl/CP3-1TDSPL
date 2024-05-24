cf50c623628ef4e97288a5ed24bbb6b84f21b2f6
igorbarrocal <igorbarrocal@hotmail.com>
2024-05-24 08:16:49 -0300

    Add o LocalStorage

--- JavaScript/login.js
+++ JavaScript/login.js
@@ -1,29 +1,32 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     
-    const form = document.querySelector('form');
+  const form = document.querySelector('form');
+
+  
+  form.addEventListener('submit', function(event) {
+    
+    event.preventDefault();
 
     
-    form.addEventListener('submit', function(event) {
-      
-      event.preventDefault();
-
-      
-      const emailInput = document.getElementById('logemail');
-      const passwordInput = document.getElementById('logpass');
+    const emailInput = document.getElementById('logemail');
+    const passwordInput = document.getElementById('logpass');
 
 
-      const email = emailInput.value.trim();
-      const password = passwordInput.value.trim();
+    const email = emailInput.value.trim();
+    const password = passwordInput.value.trim();
 
 
-      if (email === '' || password === '') {
-        alert('Por favor, preencha todos os campos.');
-        return;
-      }
+    if (email === '' || password === '') {
+      alert('Por favor, preencha todos os campos.');
+      return;
+    }
 
-      
-      
-      alert('Login bem-sucedido!');
-      window.location.href = "../index.html" ;
-    });
+    localStorage.setItem('email', email);
+    localStorage.setItem('password', password);
+    
+    alert('Login bem-sucedido!');
+    window.location.href = "../index.html";
+    const storedEmail = localStorage.getItem('email');
+
   });
+});
